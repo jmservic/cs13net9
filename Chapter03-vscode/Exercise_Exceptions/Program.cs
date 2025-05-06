ae33c0ee2e75a91db028c10e83aabc97267636c3
Sacriflces <Jmhservice@gmail.com>
2025-05-06 15:23:41 -0400

    Completed Chapter 3 exercises and began reading through Chapter 4

--- Chapter03-vscode/Exercise_Exceptions/Program.cs
+++ Chapter03-vscode/Exercise_Exceptions/Program.cs
@@ -7,10 +7,21 @@ try
 {
     byte dividend = byte.Parse(firstInput!);
     byte divisor = byte.Parse(secondInput!);
-    WriteLine(dividend);
-    WriteLine(divisor);
-} 
+    WriteLine($"{dividend} divided by {dividend} is {dividend / divisor}.");
+}
 catch (FormatException)
 {
     WriteLine("Input string was not in a correct format.");
+}
+catch (OverflowException)
+{
+    WriteLine("One of the inputs was outside of the range 0 and 255.");
+}
+catch (DivideByZeroException)
+{
+    WriteLine("You attempted to divide by zero!!");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} exception with the message {ex.Message}");
 }
