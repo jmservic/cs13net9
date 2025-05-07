be72ea6ac4d6ed12812a6182c6259cae25d6bd2b
Sacriflces <Jmhservice@gmail.com>
2025-05-07 15:01:56 -0400

    Finished the Writing Functions section

--- Chapter04-vscode/WritingFunctions/Program.Functions.cs
+++ Chapter04-vscode/WritingFunctions/Program.Functions.cs
@@ -17,7 +17,7 @@ partial class Program
 
     static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
     {
-        decimal rate = twoLetterRegionCode switch 
+        decimal rate = twoLetterRegionCode switch
         {
             "CH" => 0.08M, // SwitzerLand
             "DK" or "NO" => 0.25M, // Denmark, Norway
@@ -42,5 +42,130 @@ partial class Program
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
         }
         WriteLine($"Current Culture: {CultureInfo.CurrentCulture.DisplayName}");
+    }
+
+    /// <summary>
+    /// Pass a 32-bit unsigned integer and it will be converted into its ordinal equivalent.
+    /// </summary>
+    /// <param name="number">Number as a cardinal value e.g. 1, 2, 3, and so on.</param>
+    /// <returns>Number as an ordinal value e.g. 1st, 2nd, 3rd, and so on</returns>
+    static string CardinalToOrdinal(uint number)
+    {
+        uint lastTwoDigits = number % 100;
+
+        switch (lastTwoDigits)
+        {
+            case 11: // Special cases for 11th to 13th
+            case 12:
+            case 13:
+                return $"{number:N0}th";
+            default:
+                uint lastDigit = lastTwoDigits % 10;
+
+                string suffix = lastDigit switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th",
+                };
+
+                return $"{number:N0}{suffix}";
+        }
+    }
+
+    static void RunCardinalToOrdinal()
+    {
+        for (uint number = 1; number <= 1500; ++number)
+        {
+            Write($"{CardinalToOrdinal(number)} ");
+        }
+        WriteLine();
+    }
+
+    static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentOutOfRangeException(message: $"The factorial function is defined for non-negative integers only. Input: {number}", paramName: nameof(number));
+        }
+        else if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            checked
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+
+    static void RunFactorial()
+    {
+        for (int i = -2; i <= 15; ++i)
+        {
+            try
+            {
+                WriteLine($"{i}! = {Factorial(i):N0}");
+            }
+            catch (OverflowException)
+            {
+                WriteLine($"{i}! is too big for a 32-bit integer");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{i}! throws {ex.GetType()}: {ex.Message}");
+            }
+        }
+    }
+
+    static int FibImperative(uint term)
+    {
+        if (term == 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        else if (term == 1)
+        {
+            return 0;
+        }
+        else if (term == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return FibImperative(term - 1) + FibImperative(term - 2);    
+        }        
+    }
+
+    static int FibFunctional(uint term) => term switch 
+    {
+        0 => throw new ArgumentOutOfRangeException(),
+        1 => 0,
+        2 => 1,
+        _ => FibFunctional(term - 1) + FibFunctional(term - 2)
+    };
+
+    static void RunFibImperative()
+    {
+        for (uint i = 1; i <=30; i++)
+        {
+            WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+                arg0: CardinalToOrdinal(i),
+                arg1: FibImperative(i));
+        }
+    }
+
+    static void RunFibFunctional()
+    {
+        for (uint i = 1; i <=30; i++)
+        {
+            WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+                arg0: CardinalToOrdinal(i),
+                arg1: FibFunctional(i));
+        }
     }
 }
--- Chapter04-vscode/WritingFunctions/Program.cs
+++ Chapter04-vscode/WritingFunctions/Program.cs
@@ -1,6 +1,27 @@
 ﻿#region Times Table Example
-TimesTable(number: 7, size: 20);
+//TimesTable(number: 7, size: 20);
 #endregion
 
 #region Writing a function that returns a value
+//ConfigureConsole(culture: "fr-FR");
+
+//decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+//WriteLine($"You must pay {taxToPay:C} in tax.");
+
+//Alternatively, call the function in the itnerpolated string.
+//WriteLine($"You must pay {CaculateTax(149, "FR"):C} in tax.);
+
+#endregion
+
+#region Converting numbers from cardinal to ordinal
+//RunCardinalToOrdinal();
+#endregion
+
+#region Calculating factorials with recursion
+//RunFactorial();
+#endregion
+
+#region Using lambdas in function implementations
+//RunFibImperative();
+RunFibFunctional();
 #endregion
