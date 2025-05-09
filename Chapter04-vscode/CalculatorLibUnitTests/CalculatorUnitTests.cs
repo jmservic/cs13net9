e58c67345f366d93d3827faacc21e9a2b775fa0b
Sacriflces <Jmhservice@gmail.com>
2025-05-09 15:07:50 -0400

    Completed the ClassLIb, ExceptionHandling, Debugging, and HotReloading sections of Chapter 4

---
+++ Chapter04-vscode/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using CalculatorLib;
+
+namespace CalculatorLibUnitTests;
+
+public class CalculatorUnitTests
+{
+    [Theory]
+    [InlineData(2, 2, 4)]
+    [InlineData(2, 3, 5)]
+
+    public void Adding_Two_Doubles(double a, double b, double expected)
+    {
+        // Arrange: Set up the inputs and the unit under test.
+        var sut = new Calculator();
+
+        // Act: Execute the function to test.
+        double actual = sut.Add(a, b);
+
+        // Assert: Make assertions to compare expected to actual results
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Chapter04-vscode/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,17 @@
+﻿using CallStackExceptionHandlingLib; // To use Processor
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+
+void Beta()
+{
+    WriteLine("In Beta");
+    Processor.Gamma();
+}
