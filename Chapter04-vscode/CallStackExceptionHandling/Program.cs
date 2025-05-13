6a3281aee0610894eeee8a047976e15e2077b81b
Sacriflces <Jmhservice@gmail.com>
2025-05-13 14:17:38 -0400

    Working on the final exercisers in Ch. 4. Need to implement the PrimeFactorsApp

--- Chapter04-vscode/CallStackExceptionHandling/Program.cs
+++ Chapter04-vscode/CallStackExceptionHandling/Program.cs
@@ -13,5 +13,13 @@ void Alpha()
 void Beta()
 {
     WriteLine("In Beta");
-    Processor.Gamma();
+    try
+    {
+        Processor.Gamma();
+    }
+    catch (Exception ex) 
+    {
+        WriteLine($"Caught this: {ex.Message}");
+        throw;
+    }
 }
---
+++ Chapter04-vscode/Exercise_PrimeFactorsLib/PrimeFactors.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_PrimeFactorsLib;
+
+public static class PrimeFactors
+{
+    public static string GetPrimeFactors(int num)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(num, nameof(num));
+        
+        for (int i = num - 1; i > 1; --i)
+        {
+            if (num % i == 0)
+            {
+                int op1 = i;
+                int op2 = num / i;
+                return GetPrimeFactors(op1) + "," + GetPrimeFactors(op2);
+            }
+        }
+        return num.ToString();
+    }
+
+}
