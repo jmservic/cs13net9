2a1c502437657698e8b9832964db1ddd770fffc5
Sacriflces <Jmhservice@gmail.com>
2025-05-14 14:23:04 -0400

    Completed the PrimeFactorsApp

--- Chapter04-vscode/Exercise_PrimeFactorsApp/Program.cs
+++ Chapter04-vscode/Exercise_PrimeFactorsApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercise_PrimeFactorsLib;
+WriteLine("Welcome to the Prime factor generator!");
+string? response = "";
+do 
+{
+    try 
+    {
+        Write("Enter a number to get its prime factors or q to quit: ");
+        response = ReadLine();
+        int num = int.Parse(response!);
+        Console.WriteLine($"The prime factors of {num} are {PrimeFactors.GetPrimeFactors(num)}");
+    } catch (FormatException ex) when (response != "q") 
+    {
+        Console.WriteLine(ex.Message);
+    } catch (FormatException)
+    {
+        //do nothing!!
+    }
+} while(response != "q");
