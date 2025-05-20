32b42f2f01b3aee63143e807ef48a33b8e46b3dd
Sacriflces <Jmhservice@gmail.com>
2025-05-20 15:36:53 -0400

    Starting working on the PeopleApp section

---
+++ Chapter05-vscode/PeopleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using Packt.Shared; // To use Person.
+
+ConfigureConsole(); // Sets current culture to US English.
+// Alternatives:
+// ConfigureConsole(use ComputerCulture: true); // Use your culture.
+// ConfigureConsole(culture: "fr-FR"); // Use French culture.
+
+// Person bob = new Person(); // C# 1 or later.
+// var bob = new Person(); // C# 3 or later.
+
+Person bob = new(); // C# 9 or later.
+WriteLine(bob); // Implicit call to ToString().
+// WriteLine(bob.ToString()); // Does the same thing.
+
+bob.Name = "Bob Smith";
+bob.Born = new DateTimeOffset(
+    year: 1965, month: 12, day: 22,
+    hour: 16, minute: 28, second: 0,
+    offset: TimeSpan.FromHours(-5)); // US Eastern Standard Time.
+
+WriteLine(format: "{0} was born on {1:D}.", // Long date.
+    arg0: bob.Name, arg1: bob.Born);
+
+Person alice = new()
+{
+    Name = "Alice Jones",
+    Born = new(1998, 3, 7, 16, 28, 0,
+        // This is an optional offset from UTC time zone.
+        TimeSpan.Zero)
+};
+
+WriteLine(format: "{0} was born on {1:d}.", // Long date.
+    arg0: alice.Name, arg1: alice.Born);
+
+bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+
+WriteLine(
+    format: "{0}'s favorite wonder is {1}. Its integer is {2}.",
+    arg0: bob.Name,
+    arg1: bob.FavoriteAncientWonder,
+    arg2: (int)bob.FavoriteAncientWonder
+);
