83f0152e1edacfeb74e1affbbb67c969bfbc222d
Sacriflces <Jmhservice@gmail.com>
2025-05-23 16:29:58 -0400

    Working through passing different types of Parameters and returning different types of values, currently on Tuples.

--- Chapter05-vscode/PeopleApp/Program.cs
+++ Chapter05-vscode/PeopleApp/Program.cs
@@ -40,3 +40,137 @@ WriteLine(
     arg1: bob.FavoriteAncientWonder,
     arg2: (int)bob.FavoriteAncientWonder
 );
+
+bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+// bob.BucketList = (WondersOfTheAncientWorld)18;
+
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}.");
+
+// Works with all version of C#.
+Person alfred = new Person();
+alfred.Name = "Alfred";
+bob.Children.Add(alfred);
+
+// Works with C# 3 and later
+bob.Children.Add(new Person { Name = "Bella" });
+
+// Works with C# 9 and later
+bob.Children.Add(new() { Name = "Zoe" });
+
+WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+
+bob.Children.ForEach(x => WriteLine($"> {x.Name}"));
+
+// Or what he wrote
+// for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+// {
+//     WriteLine($"> {bob.Children[childIndex].Name}");
+// }
+
+#region Making a field static
+BankAccount.InterestRate = 0.012M; // Store a shared value in static field.
+
+BankAccount jonesAccount = new();
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+WriteLine(format: "{0} earned {1:C} interest.",
+    arg0: jonesAccount.AccountName,
+    arg1: jonesAccount.Balance * BankAccount.InterestRate);
+
+BankAccount gerrierAccount = new();
+gerrierAccount.AccountName = "Ms. Gerrier";
+gerrierAccount.Balance = 98;
+WriteLine(format: "{0} earned {1:C} interest.",
+    arg0: gerrierAccount.AccountName,
+    arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+#endregion
+
+#region Making a field constant
+// Constant fields are accessible via the type.
+WriteLine($"{bob.Name} is a {Person.Species}.");
+#endregion
+
+#region Making a field read-only
+WriteLine($"{bob.Name} was born on {bob.HomePlanet}.");
+#endregion
+
+#region Requiring fields to be set during instantiation
+
+// Instantiate a book using object initializer syntax.
+/*
+Book book = new()
+{
+    Isbn = "978-1803237800",
+    Title = "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals"
+};
+*/
+Book book = new(isbn: "978-1803237800", title: "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals")
+{
+    Author = "Mark J. Price",
+    PageCount = 821
+};
+WriteLine("{0}: {1} written by {2} has {3:N0} pages.", book.Isbn, book.Title, book.Author, book.PageCount);
+#endregion
+
+#region Initializing fields with constructors
+
+Person blankPerson = new();
+
+WriteLine(format:
+    "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+    arg0: blankPerson.Name,
+    arg1: blankPerson.HomePlanet,
+    arg2: blankPerson.Instantiated);
+
+#endregion
+
+#region Defining multiple constructors
+
+Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
+
+WriteLine(format:
+    "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+    arg0: gunny.Name,
+    arg1: gunny.HomePlanet,
+    arg2: gunny.Instantiated);
+
+#endregion
+
+#region Working with Methods and Tuples
+bob.WriteToConsole();
+WriteLine(bob.GetOrigin());
+WriteLine(bob.SayHello());
+WriteLine(bob.SayHello("Emily"));
+WriteLine(bob.OptionalParameters(3));
+WriteLine(bob.OptionalParameters(3, "Jump!", 98.5));
+
+WriteLine(bob.OptionalParameters(3, number: 52.7, command: "Hide!"));
+WriteLine(bob.OptionalParameters(3, "Poke!", active: false));
+#endregion
+
+#region Controlling how parameters are passed
+int a = 10;
+int b = 20;
+int c = 30;
+int d = 40;
+
+WriteLine($"Before: a={a}, b={b}, c={c}, d={d}");
+bob.PassingParameters(a, b, ref c, out d);
+
+WriteLine($"After: a={a}, b={b}, c={c}, d={d}");
+
+int e = 50;
+int f = 60;
+int g = 70;
+WriteLine($"Before e={e}, f={f}, g={g}, h doesn't exist yet!");
+
+// Simplified C# 7 or later syntax for the out parameter.
+bob.PassingParameters(e, f, ref g, out int h);
+WriteLine($"After: e={e}, f={f}, g={g}, h={h}");
+#endregion
+
+#region Passing a variable number of parameters
+bob.ParamsParameter("Sum using commas", 3, 6, 1, 2);
+bob.ParamsParameter("Sum using collection expression", [3, 6, 1, 2]);
+bob.ParamsParameter("Sum (empty)");
+#endregion
