2920855bb4f5783bebd913f6e88b574e5ef1daec
Sacriflces <Jmhservice@gmail.com>
2025-07-13 14:49:22 -0400

    Working on Chapter 07. Currently publishing a package to a public Nuget feed

---
+++ Chapter07-vscode/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using System.Xml.Linq; // To use XDocument.
+using System; // To use String.
+XDocument doc = new();
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
---
+++ Chapter07-vscode/SharedLibrary/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions; // To use Regex.
+
+namespace Packt.Shared;
+
+public static class StringExtensions
+{
+    public static bool IsValidXmlTag(this string input)
+    {
+        return Regex.IsMatch(input,
+        @"^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s\s+\/>)$");
+    }
+
+    public static bool IsValidPassword(this string input)
+    {
+        // Minmum of eight valid characters.
+        return Regex.IsMatch(input, "^[a-zA-Z9-9_-]{8,}$");
+    }
+}
