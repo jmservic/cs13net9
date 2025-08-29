1a99de1f0da6bf0e46aa50616d98c75add7c356f
Sacriflces <Jmhservice@gmail.com>
2025-08-29 16:12:59 -0400

    Completed Chapter 07

--- Chapter07-vscode/ConsoleApp.SourceGeneration/Program.cs
+++ Chapter07-vscode/ConsoleApp.SourceGeneration/Program.cs
@@ -1,5 +1,6 @@
-﻿ConfigureConsole();
+﻿//ConfigureConsole(culture: "fr-FR");
+ConfigureConsole(useComputerCulture: true);
 decimal price = 19.99M;
 DateTimeOffset today = DateTimeOffset.Now;
 
-WriteLine($"Today, {today:D}, the price is {price:C}");
+WriteLine($"Today, {today:D}, the price is {price:C}.");
