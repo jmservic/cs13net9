31ad057b787d1a7e28cd01d1415eacbe10fe2836
Jmservic <Jmhservice@gmail.com>
2025-10-13 15:14:47 -0400

    Completed XML / JSON Serialization in CH. 9, need to complete exercises and review online material.

---
+++ Chapter09-vscode/ControllingJson/Program.cs
@@ -0,0 +1,33 @@
+﻿using Packt.Shared; // To use Book.
+using System.Text.Json; // To use JsonSerializer.
+
+Book csharpBook = new(title: "C# 13 and .NET 9 - Modern Cross_Platform Development Fundamentals")
+{
+    Author = "Mark J Price",
+    PublishDate = new(year: 2024, month: 11, day: 12),
+    Pages = 823,
+    Created = DateTimeOffset.UtcNow,
+};
+
+JsonSerializerOptions options = new()
+{
+    IncludeFields = true,
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+};
+
+string path = Combine(CurrentDirectory, "book.json");
+
+using (Stream filestream = File.Create(path))
+{
+    JsonSerializer.Serialize(utf8Json: filestream, value: csharpBook, options);
+}
+
+WriteLine("**** File Info ****");
+WriteLine($"File: {GetFileName(path)}");
+WriteLine($"Path: {GetDirectoryName(path)}");
+WriteLine($"Size: {new FileInfo(path).Length:N0} bytes.");
+WriteLine("/------------------");
+WriteLine(File.ReadAllText(path));
+WriteLine("------------------/");
---
+++ Chapter09-vscode/WorkingWithSerialization/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Serialization; // To use [XmlAttribute].
+namespace Packt.Shared;
+
+public class Person
+{
+    public Person(decimal initialSalary)
+    {
+        Salary = initialSalary;
+    }
+
+    // A parameterless constructor is required for XML serialization.
+    public Person() { }
+
+    [XmlAttribute("fname")]
+    public string? FirstName { get; set; }
+    
+    [XmlAttribute("lname")]
+    public string? LastName { get; set; }
+
+    [XmlAttribute("dob")]
+    public DateTime DateOfBirth { get; set; }
+    public HashSet<Person>? Children { get; set; }
+    protected decimal Salary { get; set; }
+
+}
