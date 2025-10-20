9c8a8c1903b3baf418a0341638a727164cc0b7b6
Jmservic <Jmhservice@gmail.com>
2025-10-19 22:21:43 -0400

    Completed working with Environment variables and tar archives.

---
+++ Chapter09-vscode/WorkingWithEnvVars/ProgramHelpers.cs
@@ -0,0 +1,35 @@
+﻿// null namespace to merge with auto-generated Program.
+
+partial class Program
+{
+    private static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"**** {title} ****");
+        ForegroundColor = previousColor;
+    }
+
+    private static void DictionaryToTable(IDictionary dictionary)
+    {
+        Table table = new();
+        table.AddColumn("Key");
+        table.AddColumn("Value");
+
+        foreach (string key in dictionary.Keys)
+        {
+            // Some env var values are being interpreted as markup
+            try
+            {
+                table.AddRow(key, dictionary[key]!.ToString()!);
+            }
+            catch (Exception ex)
+            {
+                table.AddRow(key, ex.Message);
+                WriteLine($"{key} - {dictionary[key]}");
+            }
+        }
+
+        AnsiConsole.Write(table);
+    }
+}
