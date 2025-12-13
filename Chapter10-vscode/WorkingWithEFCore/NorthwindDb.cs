77a9fa8789e64039ddb797090429c2e6ec4aa03e
Jmservic <Jmhservice@gmail.com>
2025-12-13 14:52:39 -0500

    Working on Chapter 10 EF Core

---
+++ Chapter10-vscode/WorkingWithEFCore/NorthwindDb.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore; //To use DBContext and so on.
+using Microsoft.EntityFrameworkCore.Diagnostics; // To use RelationalEventId.
+
+namespace Northwind.EntityModels;
+
+// This manages interactions with the Northwind database.
+public class NorthwindDb : DbContext
+{
+    // These two properties map to tables in the database.
+    public DbSet<Category>? Categories {get; set;}
+    public DbSet<Product>? Products {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string databaseFile = "Northwind.db";
+        string path = Path.Combine(Environment.CurrentDirectory, databaseFile);
+
+        string connectionString = $"Data Source={path}";
+        WriteLine($"Connection: {connectionString}");
+        optionsBuilder.UseSqlite(connectionString);
+
+        optionsBuilder.LogTo(WriteLine, // This is the Console method.
+         new[] { RelationalEventId.CommandExecuting})
+        #if DEBUG
+            .EnableSensitiveDataLogging() // Include SQL parameters
+            .EnableDetailedErrors()
+        #endif
+        ;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Example of using Fluent API instead of attributes
+        // to Limit the Length of a category name to 15.
+        modelBuilder.Entity<Category>()
+        .Property(category => category.CategoryName)
+        .IsRequired() // NOT NULL
+        .HasMaxLength(15);
+
+        // Some SQLite-specific configuration.
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            // To "fix" the lack of decimal support in SQLite
+            modelBuilder.Entity<Product>()
+            .Property(product => product.Cost)
+            .HasConversion<double>();
+        }
+    }
+}
---
+++ Chapter10-vscode/WorkingWithEFCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using Northwind.EntityModels; // To use Northwind.
+
+//using NorthwindDb db = new();
+//WriteLine($"Provider: {db.Database.ProviderName}");
+// Disposes the database context.
+
+//ConfigureConsole();
+//QueryingCategories();
+
+//FilteredIncludes();
+
+//QueryingProducts();
+
+GettingOneProduct();
